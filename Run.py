796bacf09ebebe54e2f04c387a7734c9f16734bc
Lorenzo Bernardo Faella <150444533+lorenzo-faella@users.noreply.github.com>
2025-09-29 11:53:27 +0200

    Run.py

--- Run.py
+++ Run.py
@@ -46,3 +46,24 @@ def empirical_probability(N=200, M=None, K=3, iterations=100,
     
     return empirical_prob
 # %%
+
+#%%
+
+
+import SimAnn
+import importlib
+
+
+seed = 42
+
+def run3sat(N=200, M=200, mcmc_steps=100, anneal_steps=10, beta0=1, beta1=10, seed=seed, acceptance_rate=True):
+        ksat = KSAT.KSAT(N, M, 3, seed = seed)
+        
+        res = SimAnn.simann(ksat,
+                               mcmc_steps = mcmc_steps, anneal_steps = anneal_steps,
+                               beta0 = beta0, beta1 = beta1,
+                               seed = seed,
+                               debug_delta_cost = False, # set to True to enable the check
+                               acceptance_rate=acceptance_rate)
+        return res
+
